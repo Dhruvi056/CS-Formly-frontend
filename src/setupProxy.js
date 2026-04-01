5e266010fd5ad5cd3ebdd22848f65688857c08d7
Dhruvi056 <dhruviradadiya453@gmail.com>
2026-04-01 17:39:56 +0530

    Initial frontend commit

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  // Proxy API requests to Express backend
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
+
